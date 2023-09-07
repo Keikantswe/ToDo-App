99e0189447c4c8ad8f133d349abb6b31b5b56c19
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-07 10:26:11 +0200

    Project Setup

---
+++ ToDoApp/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.todoapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.todoapp to javafx.fxml;
+    exports com.example.todoapp;
+    exports controller;
+    opens controller to javafx.fxml;
+    exports;
+    opens to
+}
