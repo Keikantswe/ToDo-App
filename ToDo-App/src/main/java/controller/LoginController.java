f54866083df17698657178e64d985b4c3a43682a
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-11 10:00:14 +0200

    Jfoenix Updates
    11/09/2023
    
    On this day I updated my controller class

--- ToDo-App/src/main/java/controller/LoginController.java
+++ ToDo-App/src/main/java/controller/LoginController.java
@@ -1,14 +1,42 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXButton btnSignUp;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXPasswordField txtPassWord;
+
+    @FXML
+    void initialize() {
+        assert btnSignUp != null : "fx:id=\"btnSignUp\" was not injected: check your FXML file 'Login.fxml'.";
+        assert btnLogin != null : "fx:id=\"btnLogin\" was not injected: check your FXML file 'Login.fxml'.";
+        assert txtName != null : "fx:id=\"txtName\" was not injected: check your FXML file 'Login.fxml'.";
+        assert txtPassWord != null : "fx:id=\"txtPassWord\" was not injected: check your FXML file 'Login.fxml'.";
+
     }
 }
