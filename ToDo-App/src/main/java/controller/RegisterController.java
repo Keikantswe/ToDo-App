91236c0c98e0523c29573f5d49c81ba89059b1e7
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-12 15:53:06 +0200

    Update RegisterController.java

--- ToDo-App/src/main/java/controller/RegisterController.java
+++ ToDo-App/src/main/java/controller/RegisterController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class RegisterController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtName1;
+
+    @FXML
+    private JFXTextField txtName2;
+
+    @FXML
+    private JFXPasswordField txtPassWord;
+
+    @FXML
+    void initialize() {
+        assert txtName != null : "fx:id=\"txtName\" was not injected: check your FXML file 'Register.fxml'.";
+        assert txtName1 != null : "fx:id=\"txtName1\" was not injected: check your FXML file 'Register.fxml'.";
+        assert txtName2 != null : "fx:id=\"txtName2\" was not injected: check your FXML file 'Register.fxml'.";
+        assert txtPassWord != null : "fx:id=\"txtPassWord\" was not injected: check your FXML file 'Register.fxml'.";
+
+    }
 }
