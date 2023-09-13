d5716548e5ba4b4399e93745dadc1f14ccbef576
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-13 11:56:49 +0200

    Update RegisterController.java

--- ToDo-App/src/main/java/controller/RegisterController.java
+++ ToDo-App/src/main/java/controller/RegisterController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
@@ -19,20 +21,32 @@ public class RegisterController {
     private JFXTextField txtName;
 
     @FXML
-    private JFXTextField txtName1;
+    private JFXTextField txtlastName;
 
     @FXML
-    private JFXTextField txtName2;
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXCheckBox rdbMale;
+
+    @FXML
+    private JFXCheckBox rdbFemale;
 
     @FXML
     private JFXPasswordField txtPassWord;
 
     @FXML
+    private JFXButton btnSignUp;
+
+    @FXML
     void initialize() {
         assert txtName != null : "fx:id=\"txtName\" was not injected: check your FXML file 'Register.fxml'.";
-        assert txtName1 != null : "fx:id=\"txtName1\" was not injected: check your FXML file 'Register.fxml'.";
-        assert txtName2 != null : "fx:id=\"txtName2\" was not injected: check your FXML file 'Register.fxml'.";
+        assert txtlastName != null : "fx:id=\"txtlastName\" was not injected: check your FXML file 'Register.fxml'.";
+        assert txtUserName != null : "fx:id=\"txtUserName\" was not injected: check your FXML file 'Register.fxml'.";
+        assert rdbMale != null : "fx:id=\"rdbMale\" was not injected: check your FXML file 'Register.fxml'.";
+        assert rdbFemale != null : "fx:id=\"rdbFemale\" was not injected: check your FXML file 'Register.fxml'.";
         assert txtPassWord != null : "fx:id=\"txtPassWord\" was not injected: check your FXML file 'Register.fxml'.";
+        assert btnSignUp != null : "fx:id=\"btnSignUp\" was not injected: check your FXML file 'Register.fxml'.";
 
     }
 }
