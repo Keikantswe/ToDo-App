ca052a00028cf572bb8d8edbebe6ca2cfe3bc127
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-13 11:56:39 +0200

    Update LoginController.java

--- ToDo-App/src/main/java/controller/LoginController.java
+++ ToDo-App/src/main/java/controller/LoginController.java
@@ -4,10 +4,15 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,10 +38,43 @@ public class LoginController {
 
     @FXML
     void initialize() {
-        assert btnSignUp != null : "fx:id=\"btnSignUp\" was not injected: check your FXML file 'Login.fxml'.";
-        assert btnLogin != null : "fx:id=\"btnLogin\" was not injected: check your FXML file 'Login.fxml'.";
-        assert txtName != null : "fx:id=\"txtName\" was not injected: check your FXML file 'Login.fxml'.";
-        assert txtPassWord != null : "fx:id=\"txtPassWord\" was not injected: check your FXML file 'Login.fxml'.";
+
+        btnSignUp.setOnAction(actionEvent ->{
+            //take user to signup page
+            txtPassWord.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/Register.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        });
+
+        String userName = txtName.getText().trim();
+        String userPwd = txtPassWord.getText().trim();
+
+        btnLogin.setOnAction(actionEvent -> {
+            if(!userName.isEmpty() || !userPwd.isEmpty()){
+                LoginUser(userName, userPwd);
+            }else{
+                System.out.println("Login error");
+            }
+        });
+
+
+    }
+
+    public void LoginUser(String username, String password){
+        //Check if the user exists.
+
 
     }
 }
