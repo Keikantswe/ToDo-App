4dda578eba8e0f38e6be2329a711c45d2c3ab993
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-13 09:17:16 +0200

    Update module-info.java

--- ToDo-App/src/main/java/module-info.java
+++ ToDo-App/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.todoapp {
     requires javafx.fxml;
     requires com.jfoenix;
     requires javafx.controls;
+    requires java.sql;
 
 
     opens com.example.todoapp to javafx.fxml;
