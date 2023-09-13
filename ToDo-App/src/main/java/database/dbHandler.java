781479246b3f23ed51bf4a54171fea90139ff699
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-13 12:49:36 +0200

    Update dbHandler.java

--- ToDo-App/src/main/java/database/dbHandler.java
+++ ToDo-App/src/main/java/database/dbHandler.java
@@ -1,4 +1,5 @@
 package database;
+import java.io.FileNotFoundException;
 import java.sql.*;
 public class dbHandler extends configs{
 
@@ -15,6 +16,30 @@ public class dbHandler extends configs{
         return dbConnection;
     }
 
+    //signup user
+    public void signUp(String firstname, String lastname, String username,  String password, String gender){
+        String insert = "INSERT INTO" + constant.USER_TABLE + "("+ constant.USER_FIRSTNAME +","+ constant.USER_LASTNAME +","+ constant.USER_USERNAME +
+               ","+ constant.USER_GENDER +","+ constant.USER_PASSWORD + ")" + "VALUES(?,?,?,?,?)";
 
+        try{
+            PreparedStatement preparedStatement = getConnection().prepareStatement(insert);
+
+            preparedStatement.setString(1, firstname);
+            preparedStatement.setString(2, lastname);
+            preparedStatement.setString(3, username);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, gender);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+
+
+    }
 
 }
