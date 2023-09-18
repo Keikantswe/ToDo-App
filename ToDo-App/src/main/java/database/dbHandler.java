ad437e3508cf0fd327ab9962fb2f841ec98a2e44
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-09-18 10:32:52 +0200

    Update dbHandler.java

--- ToDo-App/src/main/java/database/dbHandler.java
+++ ToDo-App/src/main/java/database/dbHandler.java
@@ -1,4 +1,6 @@
 package database;
+import model.User;
+
 import java.io.FileNotFoundException;
 import java.sql.*;
 public class dbHandler extends configs{
@@ -17,18 +19,18 @@ public class dbHandler extends configs{
     }
 
     //signup user
-    public void signUp(String firstname, String lastname, String username,  String password, String gender){
+    public void signUp(User user){
         String insert = "INSERT INTO " + constant.USER_TABLE + "("+ constant.USER_FIRSTNAME +","+ constant.USER_LASTNAME +","+ constant.USER_USERNAME +
                ","+ constant.USER_PASSWORD +","+ constant.USER_GENDER + ")" + "VALUES(?,?,?,?,?)";
 
         try{
             PreparedStatement preparedStatement = getConnection().prepareStatement(insert);
 
-            preparedStatement.setString(1, firstname);
-            preparedStatement.setString(2, lastname);
-            preparedStatement.setString(3, username);
-            preparedStatement.setString(4, password);
-            preparedStatement.setString(5, gender);
+            preparedStatement.setString(1, user.getFirstname());
+            preparedStatement.setString(2, user.getLastname());
+            preparedStatement.setString(3, user.getUsername());
+            preparedStatement.setString(4, user.getPassword());
+            preparedStatement.setString(5, user.getGender());
 
             preparedStatement.executeUpdate();
 
